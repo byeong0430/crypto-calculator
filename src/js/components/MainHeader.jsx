73d7117cbdb5d880693595f9f485d0e21b897aa4
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-14 16:44:22 -0700

    separated main header into a separate component

---
+++ src/js/components/MainHeader.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+
+export default class MainHeader extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h6>DASHBOARD</h6>
+        <h2>OVERVIEW</h2>
+      </Fragment>
+    );
+  }
+}
